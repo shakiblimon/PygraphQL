7b9c176f17cfa56bb2bd5a6e858bc32af1dd0af3
Shakib Limon <shakib.limon@circlefintech.com>
2019-03-03 10:48:23 +0600

    Schema schema relay and model updated

---
+++ testapp/schema_relay.py
@@ -0,0 +1,51 @@
+import django_filters
+import graphene
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
+from testapp.models import Link, Vote
+
+
+class LinkFilter(django_filters.FilterSet):
+    class Meta:
+        model = Link
+        fields = ['url', 'description']
+
+
+class LinkNote(DjangoObjectType):
+    class Meta:
+        model = Link
+        interfaces = (graphene.relay.Node,)
+
+
+class VoteNote(DjangoObjectType):
+    class Meta:
+        model = Vote
+        interface = (graphene.relay.Node,)
+
+
+class RelayQuery(graphene.ObjectType):
+    relay_link = graphene.relay.Node.Field(LinkNote)
+    relay_links = DjangoFilterConnectionField(LinkNote, filterset_class=LinkFilter)
+
+
+class RelayCreateLink(graphene.relay.ClientIDMutation):
+    link = graphene.Field(LinkNote)
+
+    class Input:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate_and_get_payload(root, info, **input):
+        user = info.context.user
+
+        link = Link(
+            url=input.get('url'),description=input.get('description'),posted_by=user,
+        )
+        link.save()
+        return RelayCreateLink(link=link)
+
+
+class RelayMutation(graphene.ObjectType):
+    relay_create_link = RelayCreateLink.Field()
+
